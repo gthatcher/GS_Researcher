9314e832ff70c272a71a9dae0cf7ea89958b3eb5
geoffrey <gtaudionurse@gmail.com>
2024-08-16 13:51:11 -0700

    init

--- citation.py
+++ citation.py
@@ -5,8 +5,17 @@ class MyCitation:
         self.pArticleTitle = ""
         self.pArticleDOI = ""
         self.pArticleJournal = ""
+
+    def scrapeScholar(self):
+        SerapiSession.startsession
+        serpapi.search()
+
 class SerapiSession:
     def __init__(self):
-        self.apikey=""
-    def scrapeScholar(self):
-        serpapi.search()
+        self.apikey = ""
+        self.sessionID = ""
+
+
+    def startsession(self):
+         apikey = ""
+         self.apikey = apikey
